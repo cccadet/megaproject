b440c415f870f11ba9f8ff4a4c36922cf8186dd2
Xixo <sysacademy@sysacademy.com.br>
2017-06-11 23:54:43 -0300

    Serviços - Layout Inicial

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -6,10 +6,66 @@
 
 <template>
   <div>
-    Services
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Serviços</h1>
+        <h2 class="subtitle is-5">
+          Let this cover page describe a product or service.
+        </h2>
+        <br>
+        <div class="columns is-multiline is-mobile">
+          <!-- Coordenação de Obra -->
+          <div class="column">
+            <div class="columns box">
+              <div class="column is-5">
+                <div class="image is-1by1">
+                  <img src="https://placehold.it/500x500">
+                </div>
+              </div>
+              <div class="column is-6 ">
+                <div class="title is-6 has-text-muted">Coordenação de obra </div>
+                <hr>
+                <ul class="has-text-left">
+                  <li>Instalações Mecânicas</li>
+                  <li>Instalações Elétricas</li>
+                  <li>Construção Civil</li>
+                </ul>
+                <br>
+              </div>
+            </div>
+          </div>
+          <!-- Coordenação de Obra -->
+          <div class="column ">
+            <div class="columns box is-mobile">
+              <div class="column is-5">
+                <div class="image is-1by1">
+                  <img src="https://placehold.it/500x500">
+                </div>
+              </div>
+              <div class="column is-6 ">
+                <div class="title is-6 has-text-muted">Instalações de Equipamento</div>
+                <hr>
+                <ul class="has-text-left">
+                  <li>Instalações de Equipamentos em Geral</li>
+                  <li>Montagem Mecânica e Elétrica de EquipamentosInstalações Mecânicas</li>
+                </ul>
+                <br>
+              </div>
+            </div>
+          </div>
+          <!-- Coordenação de Obra -->
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <style scoped>
+  .title {
+    color: #4a4a4a;
+  }
 
+  .subtitle {
+    color: #4a4a4a;
+  }
 </style>
