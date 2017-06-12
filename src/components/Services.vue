aaec793f0bf51da00e50389982b67c61bdecd72e
Xixo <sysacademy@sysacademy.com.br>
2017-06-12 00:00:56 -0300

    Serviços - Layout Inicial

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -15,43 +15,69 @@
         <br>
         <div class="columns is-multiline is-mobile">
           <!-- Coordenação de Obra -->
-          <div class="column">
-            <div class="columns box">
-              <div class="column is-5">
-                <div class="image is-1by1">
-                  <img src="https://placehold.it/500x500">
+          <div class="column is-half">
+            <span class="box">
+              <div class="columns ">
+                <div class="column is-5">
+                  <div class="image is-1by1">
+                    <img src="https://placehold.it/500x500">
+                  </div>
+                </div>
+                <div class="column is-6 ">
+                  <div class="title is-6 has-text-muted">Coordenação de obra </div>
+                  <hr>
+                  <ul class="has-text-left">
+                    <li>Instalações Mecânicas</li>
+                    <li>Instalações Elétricas</li>
+                    <li>Construção Civil</li>
+                  </ul>
+                  <br>
                 </div>
               </div>
-              <div class="column is-6 ">
-                <div class="title is-6 has-text-muted">Coordenação de obra </div>
-                <hr>
-                <ul class="has-text-left">
-                  <li>Instalações Mecânicas</li>
-                  <li>Instalações Elétricas</li>
-                  <li>Construção Civil</li>
-                </ul>
-                <br>
-              </div>
-            </div>
+            </span>
           </div>
           <!-- Coordenação de Obra -->
-          <div class="column ">
-            <div class="columns box is-mobile">
-              <div class="column is-5">
-                <div class="image is-1by1">
-                  <img src="https://placehold.it/500x500">
+          <div class="column is-half">
+            <span class="box">
+              <div class="columns ">
+                <div class="column is-5">
+                  <div class="image is-1by1">
+                    <img src="https://placehold.it/500x500">
+                  </div>
+                </div>
+                <div class="column is-6 ">
+                  <div class="title is-6 has-text-muted">Coordenação de obra </div>
+                  <hr>
+                  <ul class="has-text-left">
+                    <li>Instalações Mecânicas</li>
+                    <li>Instalações Elétricas</li>
+                    <li>Construção Civil</li>
+                  </ul>
+                  <br>
                 </div>
               </div>
-              <div class="column is-6 ">
-                <div class="title is-6 has-text-muted">Instalações de Equipamento</div>
-                <hr>
-                <ul class="has-text-left">
-                  <li>Instalações de Equipamentos em Geral</li>
-                  <li>Montagem Mecânica e Elétrica de EquipamentosInstalações Mecânicas</li>
-                </ul>
-                <br>
+            </span>
+          </div>
+          <!-- Coordenação de Obra -->
+          <div class="column is-half">
+            <span class="box">
+              <div class="columns ">
+                <div class="column is-5">
+                  <div class="image is-1by1">
+                    <img src="https://placehold.it/500x500">
+                  </div>
+                </div>
+                <div class="column is-6 ">
+                  <div class="title is-6 has-text-muted">Instalações de equipamento </div>
+                  <hr>
+                  <ul class="has-text-left">
+                    <li>Instalações de Equipamentos em Geral</li>
+                    <li>Montagem Mecânica e Elétrica de EquipamentosInstalações Mecânicas</li>
+                  </ul>
+                  <br>
+                </div>
               </div>
-            </div>
+            </span>
           </div>
           <!-- Coordenação de Obra -->
         </div>
